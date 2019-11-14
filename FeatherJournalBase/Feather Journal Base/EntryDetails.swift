d3bb53841ca25a61e3aa2af22f6027552c93bef8
Mirei <mireimachiyama@gmail.com>
2019-11-14 11:40:56 -0800

    adding journal code

---
+++ FeatherJournalBase/Feather Journal Base/EntryDetails.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetails.swift
+//  Feather Journal Base
+//
+//  Created by Mirei on 11/14/19.
+//  Copyright © 2019 Feather. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
