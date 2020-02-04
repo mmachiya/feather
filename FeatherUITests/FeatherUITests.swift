abd6bc6199c7f20391615a1cbb0f6339cf045f4e
Mirei <mireimachiyama@gmail.com>
2020-02-03 18:36:09 -0800

    update swiftlint

--- FeatherUITests/FeatherUITests.swift
+++ FeatherUITests/FeatherUITests.swift
@@ -9,27 +9,26 @@
 import XCTest
 
 class FeatherUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        let app = XCUIApplication()
+        XCUIDevice.shared.orientation = UIDeviceOrientation.portrait
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // UI tests must launch the application that they test.
+    func testCancelGoogle() {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app/*@START_MENU_TOKEN@*/.buttons["GIDSignInButton"]/*[[".buttons[\"Sign in\"]",".buttons[\"GIDSignInButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.twoFingerTap()
+        app.alerts["“Feather” Wants to Use “google.com” to Sign In"].scrollViews.otherElements.buttons["Continue"].tap()
+        
     }
 
     func testLaunchPerformance() {
