1ad0349a54ffb5e856544bfd58a6853f5b72583d
ktran <kristinatran4@gmail.com>
2020-02-05 15:08:04 -0800

    added MainMenu and MyRoutine to the storyboard as well as MyRoutine.swift file

---
+++ Feather/MyRoutine.swift
@@ -0,0 +1,12 @@
+//
+//  MyRoutine.swift
+//  Feather
+//
+//  Created by Kristina Tran on 2/5/20.
+//  Copyright © 2020 CSC308_309. All rights reserved.
+//
+
+import Foundation
+class MyRoutine {
+    
+}
