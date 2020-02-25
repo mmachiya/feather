5a0e7c82c651f92b73b7b5819df6afcb85c6f084
Kaishu Miyao <34433615+kaishumiyao@users.noreply.github.com>
2020-02-25 14:28:46 -0800

    Changed json format
    Easier json iteration for ingredients list

--- WebScraping/fixjson.py
+++ WebScraping/fixjson.py
@@ -8,9 +8,20 @@ printable = {'Lu', 'Ll'}
 def filter_non_printable(str):
     return "".join(c for c in str if unicodedata.category(c) in printable).strip('*').upper()
 
-
+ndata =[]
+info = {}
 for i in range(len(data["ulta"])):
-    data["ulta"][i]["ingredients"] = [x for x in list(map(filter_non_printable, data["ulta"][i]["ingredients"])) if x]
-        
-with open("newulta.json", "w", encoding="utf8") as f:
-    json.dump(data,f, indent=4)
+    info = {
+        "name": data["ulta"][i]["name"],
+        "category": data["ulta"][i]["category"],
+        "brand": data["ulta"][i]["brand"],
+        "price": data["ulta"][i]["price"],
+        "details": data["ulta"][i]["details"],
+        "how_to_use": data["ulta"][i]["how_to_use"],
+        "ingredients":[x for x in list(map(filter_non_printable, data["ulta"][i]["ingredients"])) if x],
+        "product_url": data["ulta"][i]["product_url"]
+    }
+    ndata.append(info)
+
+with open("newulta2.json", "w", encoding="utf8") as f:
+    json.dump(ndata,f, indent=4)
