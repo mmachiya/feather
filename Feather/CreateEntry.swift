92114447400b9b5938fadf3a7a1ba0c4538c8226
Mirei <mireimachiyama@gmail.com>
2020-03-07 13:34:39 -0800

    CRIES

--- Feather/CreateEntry.swift
+++ Feather/CreateEntry.swift
@@ -27,82 +27,100 @@ class CreateEntry: UIViewController, UINavigationControllerDelegate, UIImagePick
         // Do any additional setup after loading the view.
     }
     
+    func onePerDay (){
+        let alert = UIAlertController(title: "Sorry", message: "You already selected an image!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var selfieButton: UIButton!
     @IBAction func ChooseSelfie(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source.", preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
-            if UIImagePickerController.isSourceTypeAvailable(.camera)
-            {
-                imagePicker.sourceType = .camera
-                self.present(imagePicker, animated: true, completion: nil)
-            } else {
-                print("Camera not found.")
-                let alert = UIAlertController(title: "No camera found.", message: "Camera cannot be accessed.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-        }))
-        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in imagePicker.sourceType = .photoLibrary
-            self.present(imagePicker, animated: true,  completion: nil)
-        }))
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
-        self.present(actionSheet, animated: true, completion: nil)
+        if (Calendar.current.isDateInToday(mostRecentEntryDate)){
+            onePerDay()
+        }
+        else{
+            let imagePicker = UIImagePickerController()
+                    imagePicker.delegate = self
+                    let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source.", preferredStyle: .actionSheet)
+                    actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+                        if UIImagePickerController.isSourceTypeAvailable(.camera)
+                        {
+                            imagePicker.sourceType = .camera
+                            self.present(imagePicker, animated: true, completion: nil)
+                        } else {
+                            print("Camera not found.")
+                            let alert = UIAlertController(title: "No camera found.", message: "Camera cannot be accessed.", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+            //            self.selfieButton.setTitle("YAY", for: UIControlStateNormal)
+                    }))
+                    actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in imagePicker.sourceType = .photoLibrary
+                        self.present(imagePicker, animated: true,  completion: nil)
+                    }))
+                    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+                    self.present(actionSheet, animated: true, completion: nil)
+        }
+        
         
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.originalImage] as! UIImage
-        imageView.image = image
-        print("before")
-        journalEntries[Date()] = image
-        mostRecentEntryDate = Date()
-        print (mostRecentEntryDate)
-        //ATTEMPTING TO STORE IMAGE
-        guard let data = image.jpegData(compressionQuality: 1.0)
-        else {
-            print("Error with getting image")
-            return
-        }
-        let imageName = UUID().uuidString
-        
-        let imageReference = Storage.storage().reference().child("journalFolder").child(imageName)
-        
-        imageReference.putData(data, metadata: nil) {
-            (metadata, err) in
-            if let err = err
-            {
-                print("Error " + err.localizedDescription)
-                return
-            }
-            imageReference.downloadURL(completion: {
-                (url, err) in
-                if let err = err
-                {
-                    print ("Error " + err.localizedDescription)
+        if (!Calendar.current.isDateInToday(mostRecentEntryDate)){
+            let image = info[.originalImage] as! UIImage
+                imageView.image = image
+                print("GOT IMAGE")
+                journalEntries[Date()] = image
+                mostRecentEntryDate = Date()
+                print (mostRecentEntryDate)
+                //ATTEMPTING TO STORE IMAGE
+                guard let data = image.jpegData(compressionQuality: 1.0)
+                else {
+                    print("Error with getting image")
                     return
                 }
-                guard let url = url else {
-                    print ("Error: Something wrong with url")
-                    return
+                let imageName = UUID().uuidString
+                
+                let imageReference = Storage.storage().reference().child("journalFolder").child(imageName)
+                
+                imageReference.putData(data, metadata: nil) {
+                    (metadata, err) in
+                    if let err = err
+                    {
+                        print("Error " + err.localizedDescription)
+                        return
+                    }
+                    imageReference.downloadURL(completion: {
+                        (url, err) in
+                        if let err = err
+                        {
+                            print ("Error " + err.localizedDescription)
+                            return
+                        }
+                        guard let url = url else {
+                            print ("Error: Something wrong with url")
+                            return
+                        }
+                        
+                        let dataReference = ViewController.SignUpUser.db.collection(self.collection).document(self.doc)
+                                        
+                        let urlString = url.absoluteString
+                        
+                        let currentDate = Date().description
+            
+                        ViewController.SignUpUser.journal[currentDate] = urlString
+                        
+                        dataReference.setData(["journal":[currentDate:urlString]], merge: true)
+                    })
                 }
                 
-                let dataReference = ViewController.SignUpUser.db.collection(self.collection).document(self.doc)
-                                
-                let urlString = url.absoluteString
+                print("sdasdfdsf")
                 
-                let currentDate = Date().description
-    
-                ViewController.SignUpUser.journal[currentDate] = urlString
-                
-                dataReference.setData(["journal":[currentDate:urlString]], merge: true)
-            })
+                print("ENTRIES COUNT: \(journalEntries.count)")
+                picker.dismiss(animated: true, completion: nil)
         }
         
-        print("sdasdfdsf")
         
-        print("ENTRIES COUNT: \(journalEntries.count)")
-        picker.dismiss(animated: true, completion: nil)
         
     }
     
